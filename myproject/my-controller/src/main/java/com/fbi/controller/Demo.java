6c18081d282aac89e243857557932762f96c355b
GODISBUSY <734432053@qq.com>
2023-02-24 23:41:32 +0800

    SpringBoot项目构建提交

--- myproject/my-controller/src/main/java/com/fbi/controller/Demo.java
+++ myproject/my-controller/src/main/java/com/fbi/controller/Demo.java
@@ -1,14 +1,14 @@
 package com.fbi.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Demo {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "demo";
     }
 
 }
